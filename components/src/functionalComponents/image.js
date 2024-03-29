a9361bb9261af445d2e9f8429a1bfe5600d646f6
vemaraviteja <112867097+vemaraviteja@users.noreply.github.com>
2024-03-29 12:11:43 +0530

    Delete components directory

--- components/src/functionalComponents/image.js
+++
@@ -1,9 +0,0 @@
-function Image(){
-    return(
-        <div>
-            <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt="image"/>
-        </div>
-    )
-}
-
-export default Image;
